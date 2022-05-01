288b6a23f009ba86a156443fe129024101b0d4f0
sarwar hossain <104078659+sarwar0172@users.noreply.github.com>
2022-05-01 20:04:21 +0600

    second commit

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -26,7 +26,7 @@ function HeroSection({
                     </div>
                     <div className="col">
                     <div className="home__hero-img-wrapper">
-                        <img src={img} alt={alt} className="home__hero-img" />
+                        <img src={img1} alt={alt} className="home__hero-img" />
                     </div>
                     </div>
                   
